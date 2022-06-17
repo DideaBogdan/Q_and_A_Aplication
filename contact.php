9d2f7e3011561da8fd4b0ce60cf13d79b505bda3
CosminAnd <79159337+CosminAnd@users.noreply.github.com>
2022-06-17 18:22:37 +0300

    contact+css

--- contact.php
+++ contact.php
@@ -2,7 +2,6 @@
   include_once '../Q_and_A_Aplication/api/models/Session.php';
 
   $session = new Session();
-
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +14,9 @@
   <title>Q&A - Contact</title>
   <link rel="stylesheet" href="assets/css/styles.css">
   <link rel="stylesheet" href="assets/css/contact.css">
+  <script defer src="assets/js/contact.js"></script>
 </head>
+
 <body>  
 <?php
     
@@ -42,56 +43,12 @@
     <div id="contact-title"><h3>Contact</h3></div>
     <div id="contact">
       <div class="formular">
-        <form onsubmit="sendEmail(); reset(); return false;">
+        <form id="contact" name="contact">
   
           <div id="form-col1">
           <label>Nume: </label>
           <input type="text" id="name" name="name" placeholder="Introduceti numele" required>
-          <!---!> <label for="region">Judet: </label>
-         <select id="region" name="region">
-            <option value="Alba">Alba</option>
-            <option value="Arad">Arad</option>
-            <option value="Arges">Arges</option>
-            <option value="Bacau">Bacau</option>
-            <option value="Bihor">Bihor</option>
-            <option value="Bistrita">Bistrita</option>
-            <option value="Botosani">Botosani</option>
-            <option value="Brasov">Brasov</option>
-            <option value="Braila">Braila</option>
-            <option value="Bucuresti">București</option>
-            <option value="Caras-Severin">Caraș-Severin</option>
-            <option value="Calarasi">Călărași</option>
-            <option value="Cluj">Cluj</option>
-            <option value="Constanta">Constanța</option>
-            <option value="Covasna">Covasna</option>
-            <option value="Dambovita">Dâmbovița</option>
-            <option value="Dolj">Dolj</option>
-            <option value="Galati">Galați</option>
-            <option value="Giurgiu">Giurgiu</option>
-            <option value="Gorj">Gorj</option>
-            <option value="Harghita">Harghita</option>
-            <option value="Hunedoara">Hunedoara</option>
-            <option value="Ialomita">Ialomița</option>
-            <option value="Iasi">Iasi</option>
-            <option value="Ilfov">Ilfov</option>
-            <option value="Maramures">Maramureș</option>
-            <option value="Mehedinti">Mehedinți</option>
-            <option value="Mures">Mureș</option>
-            <option value="Neamt">Neamț</option>
-            <option value="Olt">Olt</option>
-            <option value="Prahova">Prahova</option>
-            <option value="Satu Mare">Satu Mare</option>
-            <option value="Salaj">Sălaj</option>
-            <option value="Sibiu">Sibiu</option>
-            <option value="Suceava">Suceava</option>
-            <option value="Teleorman">Teleorman</option>
-            <option value="Timis">Timiș</option>
-            <option value="Tulcea">Tulcea</option>
-            <option value="Vaslui">Vaslui</option>
-            <option value="Valcea">Valcea</option>
-            <option value="Vrancea">Vrancea</option>
-          </select> 
-          <--->
+          
           <label>Email: </label>
           <input type="email" id="email" name="email" placeholder="exemplu@gmail.com" required>
           
@@ -103,45 +60,20 @@
           <label for="subject">Subiect: </label>
           <textarea id="subject" name="subject" placeholder="Informatii" style="height:200px"></textarea>
         </div>
-  
-        <div id="form-col3">
-          <input type="submit" value="Submit">
-        </div>
-      
+        <button id="button">Submit</button>
+    
         </form>
       </div>
+
       <div class="more-info">
         <h3>Pentru mai multe informatii, nu ezitati sa ne contactati prin:</h3>
         <p><strong>Telefon:</strong> +40 232 201090</p>
-        <p><strong>Mail:</strong> Q&A_everything@gmail.com</p>
+        <p><strong>Mail:</strong> qa.knowledge.bag@gmail.com</p>
         <p><strong>Fax:</strong> +40 21 243 0578</p>
       </div>
     </div>
 </section>
   
   
-
-</div>
-  <script type="text/javascript" src="assets/js/scripts.js"></script>
-  <script> src="https://smtpjs.com/v3/smtp.js" </script>
-  <script>
-    function sendEmail(){
-      Email.send({
-      Host : "smtp.gamil.com",
-      Username : "username",
-      Password : "password",
-      To : 'qa.knowledge.bag@gmail.com',
-      From : document.getElementById("email").value,
-      Subject : "New contact:",
-      Body : "Name: " + document.getElementById("name").value
-      +"<br> Email: " + document.getElementById("email").value
-      +"<br> Telefon: " + document.getElementById("phone").value
-      +"<br> Mesaj: " + document.getElementById("subject").value
-      
-      }).then(
-          message => alert(subject)
-          );
-    }
-  </script>
 </body>
 </html>
