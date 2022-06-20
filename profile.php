c6804439b4d50917b546a13d43e98d8c3ebd0628
CosminAnd <79159337+CosminAnd@users.noreply.github.com>
2022-06-20 11:53:48 +0300

    bugs-fixed

--- profile.php
+++ profile.php
@@ -13,6 +13,7 @@
     <title>User profile</title>
     <meta charset="UTF-8">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Prata&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
